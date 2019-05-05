5a799cd5685d7b674b22c71235966904fb454def
lulu.li29 <s987337613@gmail.com>
2019-05-05 15:50:07 +0800

    add wallet withdraw and store value

--- MyWalletLib/Models/Wallet.cs
+++ MyWalletLib/Models/Wallet.cs
@@ -2,5 +2,54 @@
 {
     public class Wallet
     {
+        private readonly IWalletRepo _walletRepo;
+        private readonly IBankingAccount _bankingAccount;
+        private readonly IFee _fee;
+        private readonly ILogger _log;
+
+        public Wallet(IWalletRepo walletRepo, IBankingAccount bankingAccount, IFee fee, ILogger log)
+        {
+            _walletRepo = walletRepo;
+            _bankingAccount = bankingAccount;
+            _fee = fee;
+            _log = log;
+        }
+
+        public void Withdraw(string account, decimal amount, string bankingAccount)
+        {
+            _log.Info($"account {account} amount {amount} banking account {bankingAccount}");
+            _walletRepo.UpdateDelta(account, amount * -1);
+
+            var fee = _fee.Get(bankingAccount);
+
+            _bankingAccount.Saving(bankingAccount, amount - fee);
+        }
+
+        public void StoreValue(string bankingAccount, decimal amount, string account)
+        {
+            _bankingAccount.Withdraw(bankingAccount, amount);
+            _walletRepo.UpdateDelta(account, amount);
+        }
+    }
+
+    public interface ILogger
+    {
+        void Info(string msg);
+    }
+
+    public interface IFee
+    {
+        decimal Get(string bankingAccount);
+    }
+
+    public interface IBankingAccount
+    {
+        void Saving(string bankingAccount, decimal amount);
+        void Withdraw(string bankingAccount, decimal amount);
+    }
+
+    public interface IWalletRepo
+    {
+        void UpdateDelta(string account, decimal amount);
     }
 }
--- MyWalletLibTests/WalletTests.cs
+++ MyWalletLibTests/WalletTests.cs
@@ -1,4 +1,5 @@
 ﻿using MyWalletLib.Models;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace MyWalletLibTests
@@ -6,11 +7,41 @@ namespace MyWalletLibTests
     [TestFixture]
     public class WalletTests
     {
+        private IBankingAccount _bankingAccount;
+        private IFee _fee;
+        private ILogger _log;
+        private IWalletRepo _walletRepo;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fee = Substitute.For<IFee>();
+            _log = Substitute.For<ILogger>();
+            _walletRepo = Substitute.For<IWalletRepo>();
+            _bankingAccount = Substitute.For<IBankingAccount>();
+        }
+
         [Test]
-        [Ignore("not yet")]
+        public void storedValue_from_banking_to_wallet()
+        {
+            var wallet = new Wallet(_walletRepo, _bankingAccount, _fee, _log);
+
+            wallet.StoreValue("919", 1000m, "joey");
+
+            _bankingAccount.Received(1).Withdraw("919", 1000m);
+            _walletRepo.Received(1).UpdateDelta("joey", 1000m);
+        }
+
+        [Test]
         public void withdrawal_from_wallet_to_banking_account_successfully()
         {
-            var wallet = new Wallet();
+            var wallet = new Wallet(_walletRepo, _bankingAccount, _fee, _log);
+            _fee.Get("919").ReturnsForAnyArgs(5m);
+
+            wallet.Withdraw("joey", 1000m, "919");
+
+            _walletRepo.Received(1).UpdateDelta("joey", -1000m);
+            _bankingAccount.Received().Saving("919", 995);
         }
     }
 }
